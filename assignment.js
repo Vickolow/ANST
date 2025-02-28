34ca4768e1866b904aa73a35181a6458157899ef
Aina Victor Kolawole <ainavictorkolawole@gmail.com>
2025-02-28 11:34:15 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,19 @@
+// assignmnet
+
+function calculatorOperation (num1, num2) {
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modules = num1 % num2;
+
+// Print the result 
+console.log("addition: " + num1 + " + " + num2 + " =" + addition)
+console.log("subtraction: " + num1 + " - " + num2 +" = " + subtraction)
+console.log("multiplication: " + num1 + " / " + num2 + " = " + multiplication)
+console.log("division: " + num1 + " / " + num2 + " = " + division)
+console.log("modulus: " + num1 + " % " + num2 + " = " + modules)
+}
+
+calculatorOperation(3,7)
+# 
